02330654b15a279988f37f6ed84755234acf900f
Gabriel de Lucca Vieira <bielluccabmv@gmail.com>
2018-09-21 16:43:56 -0300

    Commit 01
      - Importação de assets e uma cena em 2D para trabalhar em Network.

---
+++ Assets/Scripts/BulletScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour {
+
+	public float BulletTimer = 3f;
+
+	private Vector2 speed = new Vector2 (55, 0);
+	private Rigidbody2D rbBullet;
+
+	// Use this for initialization
+	void Start () {
+		rbBullet = GetComponent<Rigidbody2D>();
+		rbBullet.velocity = speed * this.transform.localScale.x;
+		Destroy (gameObject, BulletTimer);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/MensagemControle.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MensagemControle : MonoBehaviour {
+
+	// Use this for initialization
+	public GUIText gameOverTxt;
+	public GUIText restartTxt;
+	public GUIText venceuTxt;
+
+	public bool gameOver;
+	public bool restart;
+	public bool venceu;
+
+	void Start () {
+		gameOver = false;
+		restart = false;
+		venceu = false;
+		gameOverTxt.text = "Game Over!";
+		restartTxt.text = "Pressione 'R' para recomeçar.";
+		venceuTxt.text = "Parabéns. Você venceu!";
+	}
+	
+	void Update () {
+		if (gameOver) {
+			restartTxt.text = "Pressione 'R' para recomeçar.";
+			restart = true;
+		}
+		if (restart) {
+			if(Input.GetKeyDown(KeyCode.R))
+			{
+				SceneManager.LoadScene ("Cena1");
+				//Application.LoadLevel (Application.loadedLevel);
+			}
+		}
+	}
+
+	public void GameOver()
+	{
+		gameOverTxt.text = "Game Over!";
+		gameOver = true;
+	}
+
+	public void Venceu()
+	{
+		venceuTxt.text = "Parabéns. Você venceu!";
+		restartTxt.text = "Pressione 'R' para recomeçar.";
+		restart = true;
+	}
+}
